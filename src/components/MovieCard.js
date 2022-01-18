65ec044345d319645a78abeac748da6d5a1dd603
Ryan Gabrinao <ryan.gabrinao@gmail.com>
2022-01-18 14:48:07 -0800

    added like button component

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,23 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import noPoster from "../images/no-movie-poster.jpg";
 
 function MovieCard({ movie }) {
   return (
-    <div className="movie-card stacked">
-      <div className="movie-poster">
-        {movie.poster_path !== null ? (
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-            alt={`movie poster for ${movie.title}`}
-          />
-        ) : (
-          <img src={noPoster} alt="no poster" />
-        )}
-      </div>
-      <div className="movie-info">
-        <h2 className="movie-title">{movie.title}</h2>
-      </div>
-    </div>
+    <article>
+      <Link to={`/movie/${movie.id}`}>
+        <div className="movie-card stacked">
+          <div className="movie-poster">
+            {movie.poster_path !== null ? (
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={`movie poster for ${movie.title}`}
+              />
+            ) : (
+              <img src={noPoster} alt="no poster" />
+            )}
+          </div>
+          <div className="movie-info flex justify-between align-center gap-md">
+            <div className="right-info">
+              <h2 className="movie-title">{movie.title}</h2>
+              <span>{movie.release_date}</span>
+            </div>
+            <div className="left-info">{movie.vote_average}</div>
+          </div>
+        </div>
+      </Link>
+    </article>
   );
 }
 
