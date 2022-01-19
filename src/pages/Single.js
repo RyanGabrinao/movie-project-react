840fd5cdc4d6df9e93ad93e6701100bdc0c9a6fb
Ryan Gabrinao <ryan.gabrinao@gmail.com>
2022-01-19 10:50:33 -0800

    added poster animation for singles page

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -2,6 +2,36 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import LikeButton from "../components/LikeButton";
 import { API_KEY } from "../globals/globals";
+import { motion } from "framer-motion";
+
+// Animations
+const posterVar = {
+  init: {
+    y: 200,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1.4,
+    },
+  },
+};
+
+const imageVar = {
+  init: {
+    scale: 1.5,
+  },
+  show: {
+    scale: 1,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1.4,
+    },
+  },
+};
 
 function Single() {
   const { id } = useParams();
@@ -24,13 +54,19 @@ function Single() {
         {movie !== null && (
           <section>
             <h2>{movie.title}</h2>
-            <div className="movie-poster">
-              <img
+            <motion.div
+              className="movie-poster"
+              variants={posterVar}
+              initial="init"
+              animate="show"
+            >
+              <motion.img
+                variants={imageVar}
                 src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                 alt=""
               />
               <LikeButton />
-            </div>
+            </motion.div>
           </section>
         )}
       </main>
