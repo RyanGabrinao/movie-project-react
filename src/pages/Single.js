c42c9ac7177af9a81ae29701acfabe424aa51bcd
Ryan Gabrinao <ryan.gabrinao@gmail.com>
2022-01-20 11:05:10 -0800

    added font

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -21,21 +21,6 @@ const posterVar = {
   },
 };
 
-// const imageVar = {
-//   // init: {
-//   //   x: -200,
-//   //   opacity: 0,
-//   // },
-//   // show: {
-//   //   x: 0,
-//   //   opacity: 1,
-//   //   transition: {
-//   //     ease: [0.6, 0.01, -0.05, 0.95],
-//   //     duration: 1.4,
-//   //   },
-//   // },
-// };
-
 function Single() {
   const { id } = useParams();
   const [movie, setMovie] = useStickyState(null, `movie-${id}`);
