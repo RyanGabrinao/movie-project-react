d2b4ec87117527d93849109dfbb11990af8ad954
Ryan Gabrinao <50968744+RyanGabrinao@users.noreply.github.com>
2022-01-27 11:06:16 -0800

    updated cast cards

--- src/components/CastCard.js
+++ src/components/CastCard.js
@@ -1,12 +1,18 @@
 import React from "react";
+import noPoster from "../images/no-movie-poster.jpg";
 
 function CastCard({ cast }) {
   return (
     <div className="cast-card">
-      <img
-        src={`https://image.tmdb.org/t/p/w500/${cast.profile_path}`}
-        alt=""
-      />
+      {cast.profile_path !== null ? (
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${cast.profile_path}`}
+          alt=""
+        />
+      ) : (
+        <img src={noPoster} alt="" />
+      )}
+
       <span>{cast.name}</span>
       <span>as {cast.character}</span>
     </div>
